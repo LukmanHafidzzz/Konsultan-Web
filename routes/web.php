42a02ce3ffdba9326a78510a2355142c52f95a72
Khaerul Azmi <khaerulazmi882@gmail.com>
2023-08-31 22:31:18 +0700

    commit kedua

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+
+Route::get('/sesi',[SessionController::class, 'index']);
+
+Route::post('/sesi/login',[SessionController::class, 'login']);
+
+Route::get('/sesi/logout',[SessionController::class, 'logout']);
+
+Route::get('/sesi/register',[SessionController::class, 'register']);
+
+Route::post('/sesi/create',[SessionController::class, 'create']);
+
 });
