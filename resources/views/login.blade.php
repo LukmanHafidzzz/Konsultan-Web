2d10d1859f198257265e0ed51450bc40a300a2ee
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2023-09-01 20:12:22 +0700

    Commit Keempat

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,20 +3,39 @@
 @section('title', 'Login')
 
 @section('konten')
-    <div class="w-50 center border rounded px-3 py-3 mx-auto">
-        <h1>Login</h1>
+    <div class="center borderlogin px-3 py-3 mx-auto">
+        <h1 class="text-center text-login mb-4 mt-5">Login</h1>
         <form action="" method="POST">
             @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" value="{{ Session::get('email') }}" name="email" class="form-control">
+            <div class="ms-5 me-5">
+                <label for="email" class="form-label text-sub">E-mail</label>
+                <input type="email" value="{{ Session::get('email') }}" name="email" class="form-control form-emailLog">
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control">
+            <div class="mt-4 ms-5 me-5">
+                <label for="password" class="form-label text-sub">Password</label>
+                <input type="password" name="password" class="form-control form-emailLog">
+                <div>
+                    <div class="row mt-2 text-check">
+                        <div class="col text-start">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Remember me
+                            </label>
+                        </div>
+                        <div class="col text-end">
+                            Forgot Password
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3 d-grid"></div>
-            <button name="submit" type="submit" class="btn btn-primary">Login</button>
+            <div class="mb-3 d-grid">
+            </div>
+            <div class="d-flex justify-content-center mb-2">
+                <button name="submit" type="submit" class="btn button-login mx-auto">Login</button>
+            </div>
+            <div class="text-center text-doesntHave mb-5">
+                Doesn’t have an account? <a href="" class="text-toReg">Register</a>
+            </div>
         </form>
     </div>
 @endsection
